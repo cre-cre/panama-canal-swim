0b91eaa1b27b217c30e3ba4ccf4291be595ba21c
cre-cre <celdermire@gmail.com>
2026-01-21 15:06:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -75,4 +75,84 @@ function deleteCheck(e) {
 function filterTasks(e) {
     const tasks = taskList.childNodes;
     tasks.forEach(function(task) {
-        if
+        if (task.nodeType === 1) { // Check if it's an element node
+            switch (e.target.value) {
+                case "all":
+                    task.style.display = "flex";
+                    break;
+                case "completed":
+                    if (task.classList.contains("completed")) {
+                        task.style.display = "flex";
+                    } else {
+                        task.style.display = "none";
+                    }
+                    break;
+                case "uncompleted":
+                    if (!task.classList.contains("completed")) {
+                        task.style.display = "flex";
+                    } else {
+                        task.style.display = "none";
+                    }
+                    break;
+            }
+        }
+    });
+}
+
+function saveLocalTasks(task) {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function getTasks() {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(task) {
+        // Create task div
+        const taskDiv = document.createElement('div');
+        taskDiv.classList.add('task');
+
+        // Create li
+        const newTask = document.createElement('li');
+        newTask.innerText = task;
+        newTask.classList.add('task-item');
+        taskDiv.appendChild(newTask);
+
+        // Create Completed Button
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = '<i class="fas fa-check"></i>';
+        completedButton.classList.add('complete-btn');
+        taskDiv.appendChild(completedButton);
+
+        // Create Delete Button
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+        deleteButton.classList.add('delete-btn');
+        taskDiv.appendChild(deleteButton);
+
+        // Append to list
+        taskList.appendChild(taskDiv);
+    });
+}
+
+function removeLocalTasks(task) {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    const taskIndex = task.children[0].innerText;
+    tasks.splice(tasks.indexOf(taskIndex), 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
